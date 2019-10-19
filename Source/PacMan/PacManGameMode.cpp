070da2e7308566cb2b3f0ff5e641bb5a541d3cc5
William Ling <monkeyhood@gmail.com>
2019-10-19 14:02:18 -0400

    unreal project
    starter unreal project

---
+++ Source/PacMan/PacManGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "PacManGameMode.h"
+#include "PacManHUD.h"
+#include "PacManCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+APacManGameMode::APacManGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = APacManHUD::StaticClass();
+}
